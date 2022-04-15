6aea761e3d7e8e9c0230ed58173efc3d4af2b2df
叶鑫 <yexin@fkhwl.com>
2022-04-15 17:54:32 +0800

    路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,32 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Layout from '../layout/Layout.vue'
+import Home from '../views/Home.vue'
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: Layout
-    // redirect: '/',
+    component: Layout,
+    
+    children: [
+        {
+            path: '/',
+            component: Home,
+        },
+        {
+            path: '/menu',
+            component: () => import('../views/menu/index.vue'),
+        },
+        {
+            path: '/table',
+            component: () => import('../views/table/index.vue'),
+            children:[
+                {
+                    path: '/table/detail',
+                    name:'tabledetail',
+                    component: () => import('../views/table/detail.vue'),
+                }
+            ]
+        }
+    ]
   }
 ]
 const router = createRouter({
